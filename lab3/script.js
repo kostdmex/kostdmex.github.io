79358f6309dcb291088ecb3fc44a5c8345188465
Kamil Ostrowski <ostrowskikamil04@gmail.com>
2022-11-05 19:34:42 +0100

    Added generating blocks

--- lab3/script.js
+++ lab3/script.js
@@ -35,3 +35,45 @@ let list;
       }
     }
   });
+var buttonarea = document.querySelector('#button');
+function generate_new_block() {
+	var block = document.createElement('div');
+	block.classList.add('block');
+	var x=Math.round(0xffffff * Math.random()).toString(16);
+	var y=(6-x.length);
+	var z='000000';
+	var z1 = z.substring(0,y);
+	var color= '#' + z1 + x;
+	block.style.backgroundColor = color;
+	buttonarea.appendChild(block);
+	dragElement(block);
+}
+function dragElement(elem) {
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	elem.onmousedown = dragMouseDown;
+
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		elem.style.top = (elem.offsetTop - pos2) + "px";
+		elem.style.left = (elem.offsetLeft - pos1) + "px";
+	}
+
+	function closeDragElement() {
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+}
